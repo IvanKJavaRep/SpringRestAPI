d542de2ceea529c0f2ee4ee41bbf0fbb4453b2b1
Харитонин Иван <i.haritonin@crpt.ru>
2022-10-16 00:32:51 +0300

    custom error handler? method names and requestParam done

--- src/main/java/personal/ivan/textparseservice/restapi/MyController.java
+++ src/main/java/personal/ivan/textparseservice/restapi/MyController.java
@@ -1,37 +1,43 @@
 package personal.ivan.textparseservice.restapi;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+
 @Controller
 @RequestMapping(value = "/myservice")
 public class MyController {
 
-    @RequestMapping(value= "/{time}", method = RequestMethod.GET)
+
+
+    @RequestMapping(value = "/get/{time}", method = RequestMethod.GET)
     @ResponseBody
     public MyDTO getMyData(@PathVariable long time) {
         return new MyDTO(Calendar.getInstance(), "Это ответ метода GET!");
     }
 
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @RequestMapping(value = "/put", method = RequestMethod.PUT)
     @ResponseBody
     public MyDTO putMyData(@RequestBody MyDTO md) {
         return md;
     }
 
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
     @ResponseBody
-    public MyDTO postMyData() {
-        return new MyDTO(Calendar.getInstance(), "это ответ метода POST!");
+    public MyDTO postMyData(@RequestParam String phrase) {
+        return new MyDTO(Calendar.getInstance(), "это ответ метода POST: переданный " +
+                "параметр равен " + phrase);
     }
 
 
-    @RequestMapping(value= "/{time}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/delete/{time}", method = RequestMethod.DELETE)
     @ResponseBody
     public MyDTO deleteMyData(@PathVariable long time) {
         return new MyDTO(Calendar.getInstance(), "Это ответ метода DELETE!");
     }
+
 }
