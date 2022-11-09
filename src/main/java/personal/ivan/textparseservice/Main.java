4d973babe7c563027c61a9eba341c155212169d7
Харитонин Иван <i.haritonin@crpt.ru>
2022-11-09 21:48:50 +0300

    liquibase added

--- src/main/java/personal/ivan/textparseservice/Main.java
+++ src/main/java/personal/ivan/textparseservice/Main.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import personal.ivan.textparseservice.data.BatchUpdate;
 import personal.ivan.textparseservice.data.IMyDTORepository;
