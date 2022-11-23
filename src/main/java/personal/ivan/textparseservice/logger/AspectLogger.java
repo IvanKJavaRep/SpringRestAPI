458b06e4fff0ecb03697cdfbdeab09281a04bbf4
Харитонин Иван <i.haritonin@crpt.ru>
2022-11-23 13:54:21 +0300

    logger done

--- src/main/java/personal/ivan/textparseservice/logger/AspectLogger.java
+++ src/main/java/personal/ivan/textparseservice/logger/AspectLogger.java
@@ -1,6 +1,8 @@
 package personal.ivan.textparseservice.logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -20,7 +22,32 @@ public class AspectLogger {
             Date date = new Date();
             Timestamp timestamp = new Timestamp(date.getTime());
             FileWriter writer = new FileWriter("logs.txt", true);
-            writer.write(joinPoint.toString() + " " + timestamp + "\n");
+            writer.write("logBefore " + joinPoint.toString() + " " + timestamp + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @After("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        try {
+            Date date = new Date();
+            Timestamp timestamp = new Timestamp(date.getTime());
+            FileWriter writer = new FileWriter("logs.txt", true);
+            writer.write("logAfter " + joinPoint.toString() + " " + timestamp + "\n\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @AfterThrowing("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
+    public void logException(JoinPoint joinPoint) {
+        try {
+            Date date = new Date();
+            Timestamp timestamp = new Timestamp(date.getTime());
+            FileWriter writer = new FileWriter("logs.txt", true);
+            writer.write("logException " + joinPoint.toString() + " " + timestamp + "\n");
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
