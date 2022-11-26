2c8a7ce6600c88752b1749cd983c82ef600e8a68
Харитонин Иван <i.haritonin@crpt.ru>
2022-11-26 12:17:53 +0300

    log changed

--- src/main/java/personal/ivan/textparseservice/logger/AspectLogger.java
+++ src/main/java/personal/ivan/textparseservice/logger/AspectLogger.java
@@ -1,10 +1,8 @@
 package personal.ivan.textparseservice.logger;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.io.FileWriter;
@@ -15,8 +13,31 @@ import java.util.Date;
 @Component
 @Aspect
 public class AspectLogger {
+    @Around("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) {
+        try {
+            Date date = new Date();
+            Timestamp timestamp = new Timestamp(date.getTime());
+            FileWriter writer = new FileWriter("logs.txt", true);
+            writer.write("Method invoked " + joinPoint + "\n");
+            long beforeExecution = System.currentTimeMillis();
+            Object res = joinPoint.proceed();
+            long afterExecution = System.currentTimeMillis();
+            long result = afterExecution - beforeExecution;
+            if (result > 100) {
+                writer.write("WARNING " + joinPoint + " " + timestamp + " " + result + "\n\n");
+            } else {
+                writer.write("method finished " + joinPoint + " " + timestamp + " " + result + "\n\n");
+            }
+            writer.flush();
+            return res;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-    @Before("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
+    /*@Before("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         try {
             Date date = new Date();
@@ -27,8 +48,8 @@ public class AspectLogger {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
     @After("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
     public void logAfter(JoinPoint joinPoint) {
         try {
@@ -40,18 +61,17 @@ public class AspectLogger {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    @AfterThrowing("execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))")
-    public void logException(JoinPoint joinPoint) {
+    }*/
+
+    @AfterThrowing(pointcut = "execution(* personal.ivan.textparseservice.restapi.controller.MyController.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint,Exception ex) {
         try {
-            Date date = new Date();
-            Timestamp timestamp = new Timestamp(date.getTime());
             FileWriter writer = new FileWriter("logs.txt", true);
-            writer.write("logException " + joinPoint.toString() + " " + timestamp + "\n");
+            writer.write("Method invoked "+ joinPoint +"\n");
+            writer.write("logException " + ex.getMessage() + "\n\n");
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
