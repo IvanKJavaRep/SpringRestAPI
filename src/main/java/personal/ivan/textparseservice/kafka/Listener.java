ca35e24acf9ec3f36d1805dd092466fd314f94f2
Харитонин Иван <i.haritonin@crpt.ru>
2022-12-29 13:19:58 +0300

    retyr done

--- src/main/java/personal/ivan/textparseservice/kafka/Listener.java
+++ src/main/java/personal/ivan/textparseservice/kafka/Listener.java
@@ -1,7 +1,9 @@
 package personal.ivan.textparseservice.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import personal.ivan.textparseservice.restapi.dto.MyDTO;
 import personal.ivan.textparseservice.service.MyTableService;
+
+import java.security.InvalidParameterException;
 
 @Service
 public final class Listener {
@@ -32,6 +36,7 @@ public final class Listener {
             logger.info(String.format("$$$$ => Consumed message: %s", message));
             MyDTO obj = objectMapper.readValue(message, MyDTO.class);
             myTableService.createEntity(obj);
+            throw new RuntimeException();
         } catch (JsonMappingException ex) {
             System.out.println("Invalid data");
         } catch (Exception ex) {
@@ -48,14 +53,17 @@ public final class Listener {
     public void retry(String message, Acknowledgment ack) {
         try {
             System.out.println(message);
-            Thread.sleep(500);
             MyDTO obj = objectMapper.readValue(message, MyDTO.class);
-            myTableService.createEntity(obj);
         } catch (Exception ex) {
             System.out.println("problems with network try in 2 seconds");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if (retryCounter < 5) {
                 retryCounter += 1;
-                kafkaTemplate.send("retryTopic", message);
+                throw new RuntimeException();
             } else {
                 retryCounter = 0;
             }
